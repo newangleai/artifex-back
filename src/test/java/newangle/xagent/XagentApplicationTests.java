c4f61ed6c5a8faa2513ec5fb2a708e01d46e1bf8
Luiz Fernando <luizfernandosant26@gmail.com>
2025-08-13 16:52:45 -0300

    project created

---
+++ src/test/java/newangle/xagent/XagentApplicationTests.java
@@ -0,0 +1,13 @@
+package newangle.xagent;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class XagentApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
