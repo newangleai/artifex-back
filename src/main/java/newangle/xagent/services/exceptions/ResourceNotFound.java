ddbef6bb7010992fcbad4dcf434ce55b8d2f76a1
Luiz Fernando <luizfernandosant26@gmail.com>
2025-08-21 16:56:55 -0300

    treatment for id not found exception

---
+++ src/main/java/newangle/xagent/services/exceptions/ResourceNotFound.java
@@ -0,0 +1,9 @@
+package newangle.xagent.services.exceptions;
+
+public class ResourceNotFound extends RuntimeException {
+
+    public ResourceNotFound(Object id) {
+		super("Resource not found. Id " + id);
+	}
+
+}
