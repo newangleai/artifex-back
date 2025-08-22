87ad50392bcf4abed226143246ad50f2dab54f82
Luiz Fernando <luizfernandosant26@gmail.com>
2025-08-22 11:14:25 -0300

    feat/ delete agent by UUID

--- src/main/java/newangle/xagent/controllers/AiAgentController.java
+++ src/main/java/newangle/xagent/controllers/AiAgentController.java
@@ -5,6 +5,7 @@ import java.util.UUID;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -32,6 +33,12 @@ public class AiAgentController {
     public ResponseEntity<AiAgent> findById(@PathVariable UUID id) {
         AiAgent aiAgent = service.findById(id);
         return ResponseEntity.ok().body(aiAgent);
+    }
+
+    @DeleteMapping(value="/agents/{id}")
+    public ResponseEntity<AiAgent> deleteAgent(@PathVariable UUID id) {
+        service.deleteAgent(id);
+        return ResponseEntity.noContent().build();
     }
 
 }
--- src/main/java/newangle/xagent/services/AiAgentService.java
+++ src/main/java/newangle/xagent/services/AiAgentService.java
@@ -39,4 +39,8 @@ public class AiAgentService {
         return obj.orElseThrow(() -> new ResourceNotFound(id));
     }
 
+    public void deleteAgent(UUID id) {
+        aiAgentrepository.deleteById(id);
+    }
+
 }
