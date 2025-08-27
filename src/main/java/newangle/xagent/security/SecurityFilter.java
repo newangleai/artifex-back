9d2fb1c7849f4c3384a9de107df08b04d4bb8f05
Luiz Fernando <luizfernandosant26@gmail.com>
2025-08-27 13:58:57 -0300

    add NonNull notation in function parameters

--- src/main/java/newangle/xagent/security/SecurityFilter.java
+++ src/main/java/newangle/xagent/security/SecurityFilter.java
@@ -3,6 +3,7 @@ package newangle.xagent.security;
 import java.io.IOException;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.lang.NonNull;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.core.userdetails.UserDetails;
@@ -25,7 +26,7 @@ public class SecurityFilter extends OncePerRequestFilter {
     private UserRepository userRepository;
 
     @Override
-    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain)
+    protected void doFilterInternal(@NonNull HttpServletRequest request, @NonNull HttpServletResponse response, @NonNull FilterChain filterChain)
             throws ServletException, IOException {
                 var token = this.recoverToken(request);
                 if (token != null) {
