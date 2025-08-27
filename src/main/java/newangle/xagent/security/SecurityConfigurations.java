4d9e4112086a29d2b7d1de208352ec5250dc0b0b
Luiz Fernando <luizfernandosant26@gmail.com>
2025-08-26 21:59:20 -0300

    feat/ request matchers on filter chain

--- src/main/java/newangle/xagent/security/SecurityConfigurations.java
+++ src/main/java/newangle/xagent/security/SecurityConfigurations.java
@@ -2,6 +2,7 @@ package newangle.xagent.security;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
@@ -16,6 +17,10 @@ public class SecurityConfigurations {
         return http
                 .csrf(csrf -> csrf.disable())  
                 .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+                .authorizeHttpRequests(authorize -> authorize
+                    .requestMatchers(HttpMethod.GET, "/users/**").hasRole("ADMIN")
+                    .anyRequest().authenticated()
+                )
                 .build();
     }
 }
