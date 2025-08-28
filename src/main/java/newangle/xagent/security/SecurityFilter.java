910ca58420b02db15e0f0bd2a959fed1a1cb932d
Luiz Fernando <luizfernandosant26@gmail.com>
2025-08-28 12:46:17 -0300

    fix/ Security Filter Errors

--- src/main/java/newangle/xagent/security/SecurityFilter.java
+++ src/main/java/newangle/xagent/security/SecurityFilter.java
@@ -28,16 +28,26 @@ public class SecurityFilter extends OncePerRequestFilter {
     @Override
     protected void doFilterInternal(@NonNull HttpServletRequest request, @NonNull HttpServletResponse response, @NonNull FilterChain filterChain)
             throws ServletException, IOException {
-                var token = this.recoverToken(request);
-                if (token != null) {
-                    var username = tokenService.validateToken(token);
-                    UserDetails user = userRepository.findByUsername(username);
+            
+        var token = this.recoverToken(request);
 
+        if (token != null) {
+            try {
+                var username = tokenService.validateToken(token);
+                UserDetails user = userRepository.findByUsername(username);
+
+                if (user != null) {
                     var authentication = new UsernamePasswordAuthenticationToken(user, null, user.getAuthorities());
                     SecurityContextHolder.getContext().setAuthentication(authentication);
                 }
+            } catch (Exception e) {
+                // In case of invalid, expired token or any other error, "catch" catches the exception and does nothing.
+                // The request will proceed without authentication and will be barred later if the endpoint is protected, 
+                // resulting in the correct 401 Unauthorized.
+            }
+        }
 
-                filterChain.doFilter(request, response);
+        filterChain.doFilter(request, response);
     }
 
     private String recoverToken(HttpServletRequest request) {
